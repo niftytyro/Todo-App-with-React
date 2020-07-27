611de7789f7dd5351c7c1f805c5f7161efa3771b
udasitharani <udasitharani@users.noreply.github.com>
2020-07-27 20:19:25 +0000

    deploy: a8dbe5ed6c81c83a9f2002ee857b31e009155dc0

---
+++ precache-manifest.f4ddf50b5419c33e24a8e2a60664c014.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ffef90dad973dda072b778a53a4bb9f",
+    "url": "/Todo-App-with-React/index.html"
+  },
+  {
+    "revision": "0d473f900aecd1f068b0",
+    "url": "/Todo-App-with-React/static/css/main.065ba317.chunk.css"
+  },
+  {
+    "revision": "994b34c083bf40e82d66",
+    "url": "/Todo-App-with-React/static/js/2.3277913d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Todo-App-with-React/static/js/2.3277913d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d473f900aecd1f068b0",
+    "url": "/Todo-App-with-React/static/js/main.54be07ee.chunk.js"
+  },
+  {
+    "revision": "06f75fb272f423ae04bb",
+    "url": "/Todo-App-with-React/static/js/runtime-main.0b5cfe79.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1117d62ff0f78d067226c6f7e8347efd.js"
+  "/Todo-App-with-React/precache-manifest.f4ddf50b5419c33e24a8e2a60664c014.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Todo-App-with-React/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
