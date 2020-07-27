9a93177eecb8c57f98d03e74bf0480915671c3a8
udasitharani <udasitharani@gmail.com>
2020-07-28 00:39:53 +0530

    finalize todo-list component

--- src/App.js
+++ src/App.js
@@ -4,12 +4,17 @@ import HeaderComponent from "./Components/HeaderComponent";
 import TodosComponent from "./Components/Todos";
 
 class App extends Component {
+  state = {
+    tasks: ["Task #1", "Task #2", "Task #3", "Task #4", "Task #5"],
+  };
+
   render() {
+    let todosList = [...this.state.tasks];
     return (
       <div className="App">
         <HeaderComponent></HeaderComponent>
         <div className="todo-container">
-          <TodosComponent></TodosComponent>
+          <TodosComponent todosList={todosList}></TodosComponent>
         </div>
       </div>
     );
--- src/Components/Todos.js
+++ src/Components/Todos.js
@@ -1,7 +1,15 @@
 import React from "react";
+import "./Todos.css";
 
-const todos = () => {
-  return <div>hello</div>;
+const todos = (props) => {
+  var todosList = props.todosList.map((value, index) => {
+    return (
+      <li className="todo-item" key={index}>
+        {value}
+      </li>
+    );
+  });
+  return <ul className="todos-list">{todosList}</ul>;
 };
 
 export default todos;
