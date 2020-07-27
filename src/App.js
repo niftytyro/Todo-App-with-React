8802df3198a546648eb008b29198cf4e59c5aeff
udasitharani <udasitharani@gmail.com>
2020-07-28 00:27:10 +0530

    convert app to class-based component and intial setup todos component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
-import React from "react";
+import React, { Component } from "react";
 import "./App.css";
-import HeaderComponent from "./Components/Header Component";
+import HeaderComponent from "./Components/HeaderComponent";
+import TodosComponent from "./Components/Todos";
 
-function App() {
-  return (
-    <div className="App">
-      <HeaderComponent></HeaderComponent>
-      <div className="todo-container"></div>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <HeaderComponent></HeaderComponent>
+        <div className="todo-container">
+          <TodosComponent></TodosComponent>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/Components/Todos.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const todos = () => {
+  return <div>hello</div>;
+};
+
+export default todos;
